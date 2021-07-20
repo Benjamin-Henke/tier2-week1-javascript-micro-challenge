244f8b38d9ce824939019ba35297fb935707e493
Benjamin Henke <benjaminmhenke@gmail.com>
2021-07-20 06:00:57 -0500

    Completed 100-Addition

--- client.js
+++ client.js
@@ -1,1 +1,22 @@
-console.log('JS');
+console.log('**JS Micro Challenge**');
+
+// 100-Addition
+// Create a function that takes two numbers and return the sum
+function sumOfTwoNumbers (numberOne, numberTwo) {
+  let sum = numberOne + numberTwo;
+  return sum;
+} // end sumOfTwoNumbers
+
+console.log('100-Addiiton');
+console.log('5 + 5 equals =',sumOfTwoNumbers(5, 5));            // expected output 10  
+console.log('13 + 15 equals =',sumOfTwoNumbers(13, 15));        // expected output 28
+console.log('2 + 54 equals =',sumOfTwoNumbers(2, 54));          // expected output 56
+
+
+// 110-Conditional
+// Create a function that checks two numbers.
+// If the first number is bigger, return 'The first number was was bigger!'.
+// if the second number is bigger, return 'The second number was bigger!'.
+// If they are the same, return 'The numbers are the same!'.
+
+console.log('110-Conditonal');
